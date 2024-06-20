9c8b2e28c102641379072eb39b1484cced2ca6ed
1 <maxim.talag@gmail.com>
2024-06-20 20:49:43 +0300

    init

---
+++ src/main/java/ru/star/startask/StartaskApplication.java
@@ -0,0 +1,13 @@
+package ru.star.startask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/star/startask/StartaskApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.star.startask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
